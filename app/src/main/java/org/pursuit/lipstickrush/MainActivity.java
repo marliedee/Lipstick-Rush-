862f96896b31efa814eb8b9ac1ad9eda932a49df
marliedee <marlyd364@gmail.com>
2019-04-01 21:35:02 -0400

    added menu options

--- app/src/main/java/org/pursuit/lipstickrush/MainActivity.java
+++ app/src/main/java/org/pursuit/lipstickrush/MainActivity.java
@@ -1,7 +1,11 @@
 package org.pursuit.lipstickrush;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,4 +30,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.linkedIn:
+                Intent linkedIn = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/marliedee/"));
+                startActivity(linkedIn);
+                return true;
+            case R.id.github:
+                Intent github = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/marliedee"));
+                startActivity(github);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
